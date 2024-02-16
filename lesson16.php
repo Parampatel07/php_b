9ba99444b446f6ab719d9497d1f38e81b8a47db3
parampatel07 <iamparam102@gmail.com>
2024-02-16 17:06:18 +0530

    php

---
+++ lesson16.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = 'Param patel ';
+$age = 20;
+
+$text = sprintf('Hello i am %s and i am %d years old', $name, $age);
+
+echo $text;
+
+$name = '   Param Patel ';
+trim($name);
+echo $name;
+?>
