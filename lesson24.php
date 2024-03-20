b756dac3116f1a64176083f8779c48c8093d259b
parampatel07 <iamparam102@gmail.com>
2024-03-20 16:55:39 +0530

    php

--- lesson24.php
+++ lesson24.php
@@ -7,9 +7,7 @@
      <title>Document</title>
 </head>
 <body>
-     
      <form action="file_upload.php" method="post" enctype="multipart/form-data">
-
           <label for="">Select any one image </label>
           <input type="file" name="image1" id="image1" required>
 
---
+++ lesson25.php
@@ -0,0 +1,9 @@
+<?php
+
+require 'lesson26.php';
+echo '<br>';
+echo 'This is content ';
+echo '<br>';
+include 'lesson27.php';
+
+?>
